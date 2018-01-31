155c59227ea8ddc259b4d07df7f5b9025820bfa6
Rey Tang <tang.changrey@gmail.com>
2018-01-30 21:11:03 -0800

    init

---
+++ gaussian.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.spatial.distance as dist
+from numpy import random
+from scipy.stats import norm
+
+def normalize(pt):
+    return pt/np.linalg.norm(pt)
+
+def angle_between(pt1, pt2):
+    pt1_u = normalize(pt1)
+    pt2_u = normalize(pt2)
+    return np.arccos(np.clip(np.dot(pt1_u, pt2_u), -1.0, 1.0))
+
+def plot(data, title):
+    mu, std = norm.fit(data)
+    plt.hist(data, bins=25, normed=True, alpha=0.6, color='g')
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 100)
+    p = norm.pdf(x, mu, std)
+    plt.plot(x, p, 'k', linewidth=2)
+    cap = "N = %d,  d = %d,  mu = %.2f,  std = %.2f" % (N, d, mu, std)
+    plt.title(title + "\n" + cap)
+    plt.show()
+
+def dist_from_origin(rand_pts):
+    origin = np.zeros(d, int)
+    data = list()
+    for pt in rand_pts:
+        data.append(dist.euclidean(origin, pt))
+    plot(data, "Distribution of Distance from Origin")
+
+def dist_from_other_pts(rand_pts):
+    data = list()
+    for i in range(0,N*100):
+        pt1 = rand_pts[random.randint(N)]
+        pt2 = rand_pts[random.randint(N)]
+        data.append(dist.euclidean(pt1, pt2))
+    plot(data, "Distribution of Distance Between Pairs")
+
+def angle_with_other_pts(rand_pts):
+    data = list()
+    for i in range(0,N*100):
+        pt1 = rand_pts[random.randint(N)]
+        pt2 = rand_pts[random.randint(N)]
+        data.append(angle_between(pt1, pt2))
+    plot(data, "Distribution of Angle Between Pairs")
+
+def num_points_in_unit_ball(rand_pts):
+    origin = np.zeros(d, int)
+    num_inside = 0
+    for pt in rand_pts:
+        if dist.euclidean(origin, pt) < 1:
+            num_inside += 1
+    print("There are %d/%d points inside the unit ball, roughly %f percent" % (num_inside, N, num_inside/float(N)))
+
+def num_points_in_sqrt_d_ball(rand_pts):
+    origin = np.zeros(d, int)
+    sqrtd = math.sqrt(d)
+    num_inside = 0
+    for pt in rand_pts:
+        if dist.euclidean(origin, pt) < sqrtd:
+            num_inside += 1
+    print("There are %d/%d points inside the ball of radius sqrt(d), roughly %f percent" % (num_inside, N, num_inside/float(N)))
+
+def main():
+    mean = np.zeros(d, int)
+    cov = np.zeros((d,d), int)
+    np.fill_diagonal(cov, 1)
+
+    origin = np.zeros(d, int)
+    rand_pts = list()
+
+    for i in range(0,N):
+        rand_pts.append(random.multivariate_normal(mean, cov))
+
+    if type == "dist_origin":
+        dist_from_origin(rand_pts)
+    elif type == "dist_pair":
+        dist_from_other_pts(rand_pts)
+    elif type == "angle":
+        angle_with_other_pts(rand_pts)
+    elif type == "unit_ball":
+        num_points_in_unit_ball(rand_pts)
+    elif type == "sqrtd_ball":
+        num_points_in_sqrt_d_ball(rand_pts)
+    else:
+        print("USAGE: ./gaussian.py N d {dist_origin, dist_pair, angle, unit_ball, sqrtd_ball}")
+
+    exit(1)
+
+if len(sys.argv) != 4:
+    print("USAGE: ./gaussian.py N d {dist_origin, dist_pair, angle, unit_ball, sqrtd_ball}")
+    exit(1)
+
+N = int(sys.argv[1])
+d = int(sys.argv[2])
+type = sys.argv[3]
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ projection.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.spatial.distance as dist
+from numpy import random
+from scipy.stats import norm
+import decimal
+
+def calculate_distance_between_all_pairs(pts):
+    dist_between_pts = list()
+    for i in range(0,20):
+        for j in range(i+1,20):
+            dist_between_pts.append(dist.euclidean(pts[i],pts[j]))
+    return dist_between_pts
+
+def project_data(pts, d, k):
+    mean = np.zeros(d, int)
+    cov = np.zeros((d,d), int)
+    np.fill_diagonal(cov, 1)
+    ulist = list()
+    for i in range(0, k):
+        ulist.append(random.multivariate_normal(mean,cov))
+    projected_data = list()
+    for pt in pts:
+        projected_pt = list()
+        for u in ulist:
+            projected_pt.append(np.dot(pt,u))
+        #print(str(np.linalg.norm(projected_pt)) + ":" + str(math.sqrt(k)*np.linalg.norm(pt)))
+        projected_data.append(np.array(projected_pt))
+    return projected_data
+
+def find_max_difference(orig_dist, proj_dist, k):
+    sqrtk = math.sqrt(k)
+    sqrtk_orig_dist = [d*sqrtk for d in orig_dist]
+    max_diff = 0
+    for i in range(0,len(orig_dist)):
+        cur_diff = abs(sqrtk_orig_dist[i] - proj_dist[i])
+        if (cur_diff > max_diff):
+            max_diff = cur_diff
+    print("The maximum difference is %d which is roughly %f percent of k=%d" % (max_diff, max_diff/k, k))
+
+
+def main():
+    d = 900
+    r = 30
+    origin = np.zeros(d, int)
+    mean = np.zeros(d, int)
+    cov = np.zeros((d,d), int)
+    np.fill_diagonal(cov, 1)
+
+    origin = np.zeros(d, int)
+    rand_pts = list()
+
+    while len(rand_pts) != 20:
+        rand_pt = random.multivariate_normal(mean,cov)
+        if dist.euclidean(origin, rand_pt) < r:
+            rand_pts.append(rand_pt)
+
+    #print(rand_pts)
+    original_dists = calculate_distance_between_all_pairs(rand_pts)
+    projected_100 = project_data(rand_pts, d, 100)
+    projected_50 = project_data(rand_pts, d, 50)
+    projected_10 = project_data(rand_pts, d, 10)
+    projected_5 = project_data(rand_pts, d, 5)
+    projected_4 = project_data(rand_pts, d, 4)
+    projected_3 = project_data(rand_pts, d, 3)
+    projected_2 = project_data(rand_pts, d, 2)
+    projected_1 = project_data(rand_pts, d, 1)
+    #print(projected_100)
+    dists_100 = calculate_distance_between_all_pairs(projected_100)
+    dists_50 = calculate_distance_between_all_pairs(projected_50)
+    dists_10 = calculate_distance_between_all_pairs(projected_10)
+    dists_5 = calculate_distance_between_all_pairs(projected_5)
+    dists_4 = calculate_distance_between_all_pairs(projected_4)
+    dists_3 = calculate_distance_between_all_pairs(projected_3)
+    dists_2 = calculate_distance_between_all_pairs(projected_2)
+    dists_1 = calculate_distance_between_all_pairs(projected_1)
+    #print(dists_100)
+    find_max_difference(original_dists, dists_100, 100)
+    find_max_difference(original_dists, dists_50, 50)
+    find_max_difference(original_dists, dists_10, 10)
+    find_max_difference(original_dists, dists_5, 5)
+    find_max_difference(original_dists, dists_4, 4)
+    find_max_difference(original_dists, dists_3, 3)
+    find_max_difference(original_dists, dists_2, 2)
+    find_max_difference(original_dists, dists_1, 1)
+
+if __name__ == "__main__":
+    main()
