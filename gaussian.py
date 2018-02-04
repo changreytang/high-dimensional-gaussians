4734cf779c652d4c50e7b9cd0640bfee65d23e56
Rey Tang <tang.changrey@gmail.com>
2018-02-04 15:20:26 -0800

    lol

--- gaussian.py
+++ gaussian.py
@@ -103,5 +103,3 @@ type = sys.argv[3]
 
 if __name__ == "__main__":
     main()
-
-
--- projection.py
+++ projection.py
@@ -18,22 +18,8 @@ def calculate_distance_between_all_pairs(pts):
     return dist_between_pts
 
 def project_data(pts, d, k):
-    #mean = np.zeros(d, int)
-    #cov = np.zeros((d,d), int)
-    #np.fill_diagonal(cov, 1)
-    #ulist = list()
-    #for i in range(0, k):
-    #    ulist.append(random.multivariate_normal(mean,cov))
-    #projected_data = list()
-    #for pt in pts:
-    #    projected_pt = list()
-    #    for u in ulist:
-    #        projected_pt.append(np.dot(pt,u))
-        #print(str(np.linalg.norm(projected_pt)) + ":" + str(math.sqrt(k)*np.linalg.norm(pt)))
-    #    projected_data.append(np.array(projected_pt))
     transformer = random_projection.GaussianRandomProjection(k)
     projected_data = transformer.fit_transform(pts)
-    #print(projected_data.shape)
     return projected_data
 
 def find_max_difference(orig_dist, proj_dist, k):
@@ -63,11 +49,9 @@ def main():
         if dist.euclidean(origin, rand_pt) < r:
             rand_pts.append(rand_pt)
 
-    #print(rand_pts)
     original_dists = calculate_distance_between_all_pairs(rand_pts)
-    #print(original_dists)
+
     projected_100 = project_data(rand_pts, d, 100)
-    #print(projected_100)
     projected_50 = project_data(rand_pts, d, 50)
     projected_10 = project_data(rand_pts, d, 10)
     projected_5 = project_data(rand_pts, d, 5)
@@ -75,9 +59,8 @@ def main():
     projected_3 = project_data(rand_pts, d, 3)
     projected_2 = project_data(rand_pts, d, 2)
     projected_1 = project_data(rand_pts, d, 1)
-    #print(projected_100)
+
     dists_100 = calculate_distance_between_all_pairs(projected_100)
-    #print(dists_100)
     dists_50 = calculate_distance_between_all_pairs(projected_50)
     dists_10 = calculate_distance_between_all_pairs(projected_10)
     dists_5 = calculate_distance_between_all_pairs(projected_5)
@@ -85,7 +68,7 @@ def main():
     dists_3 = calculate_distance_between_all_pairs(projected_3)
     dists_2 = calculate_distance_between_all_pairs(projected_2)
     dists_1 = calculate_distance_between_all_pairs(projected_1)
-    #print(dists_100)
+
     find_max_difference(original_dists, dists_100, 100)
     find_max_difference(original_dists, dists_50, 50)
     find_max_difference(original_dists, dists_10, 10)
